9c92f061821abd83b9d91e51cf3b55f6a14a3a2b
Lucas <lucashuati@gmail.com>
2017-04-07 11:31:21 -0300

    delete files

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^logout/$', views.logout_user, name="logout"),
-    url(r'^new/$', views.new_user, name="newuser"),
-
-]
